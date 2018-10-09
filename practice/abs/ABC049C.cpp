1f0595860a7d8f71d80b541663bfc7f615837730
Tirol_JPN <Tirol.JPN@gmail.com>
2018-10-09 16:22:55 +0900

    edit abs file

--- practice/abs/ABC049C.cpp
+++ practice/abs/ABC049C.cpp
@@ -4,17 +4,25 @@ using namespace std;
 
 string s;
 int index = 0;
-
-bool check(index, t_elem){
-
-}
+int lnegth;
+bool flag  = false;
 
 int main (){
     cin >> s;
-
-    int lnegth = s.size();
-    bool flag = true;
-    while(index != length){
-
+    length = s.size();
+    /*
+     * 文字列の被りの可能性があるTに追加する文字列の組み合わせ
+     * dream　dreamer erase eraser
+     * dream(er) erase(r)
+     */
+    while(true){
+        if(index == s.size()){
+            flag = true;
+            break;
+        }
+        // 次の探索対象が"dream"と等しければ
+        if(s.compare(index, index+5, 'dream' ) == 0){
+            index += 5;
+        }
     }
 }
