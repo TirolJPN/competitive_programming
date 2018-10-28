71bfe0d710544b8fffa405865ba8145f055c282a
Tirol_JPN <Tirol.JPN@gmail.com>
2018-10-28 14:53:29 +0900

    abs追加(compare関数の使い方がゴミ)

--- practice/abs/ABC049C.cpp
+++ practice/abs/ABC049C.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 string s;
-int index = 0;
-int lnegth;
-bool flag  = false;
+int length;
+
+int cmp_str (int i, string str){
+    return s.compare(i, str.size(), str );
+}
 
 int main (){
     cin >> s;
     length = s.size();
-    /*
-     * 文字列の被りの可能性があるTに追加する文字列の組み合わせ
-     * dream　dreamer erase eraser
-     * dream(er) erase(r)
-     */
-    while(true){
-        if(index == s.size()){
-            flag = true;
+    int index=0;
+
+//  入力文字列の順序を逆にする
+    reverse(s.begin(), s.end());
+
+    string dream("maerd");
+    string dreamer("remaerd");
+    string erase("esare");
+    string eraser("resare");
+
+
+//  全ての文字列を読み終わるまで続ける
+    while(index < length){
+        if(cmp_str(index, dream) == 0){
+            index +=5;
+            continue;
+        }else if(cmp_str(index, dreamer)  == 0){
+            index +=7;
+            continue;
+        }else if(cmp_str(index, erase) == 0){
+            index +=5;
+            continue;
+        }else if(cmp_str(index, eraser) == 0){
+            index +=6;
+            continue;
+        }else{
             break;
         }
-        // 次の探索対象が"dream"と等しければ
-        if(s.compare(index, index+5, 'dream' ) == 0){
-            index += 5;
-        }
     }
+    if(index == length){
+        cout << "YES" << '\n';
+    }else{
+        cout << "NO" << '\n';
+    }
+
+    return 0;
 }
