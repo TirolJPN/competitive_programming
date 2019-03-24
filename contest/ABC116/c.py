9104e9d637ee8b5f70cdd071490de46939ee1a5d
Tirol_JPN <Tirol.JPN@gmail.com>
2019-03-24 18:22:16 +0900

    ABC116 C submission

--- contest/ABC116/c.py
+++ contest/ABC116/c.py
@@ -1,3 +1,27 @@
-N, M= (int(i) for i in input().split())
+N = int(input())
 H = list(int(i) for i in input().split())
 
+# 再帰的にカウントしていく
+def count(h):
+    print(h)
+    if len(h) != 0:
+        output = 0
+        min_elem = min(h)
+        output += min_elem
+        for index in range(0, len(h)):
+            h[index] -= min_elem
+        tmp_h = []
+        for index in range(0, len(h)):
+            if (h[index] > 0):
+                tmp_h.append(h[index])
+            else:
+                output += count(tmp_h)
+                tmp_h = []
+        output += count(tmp_h)
+        print(output)
+        return output
+    else:
+        return 0
+
+h = H
+print(count(h))
