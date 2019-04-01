15d43c7c27ebcfbd9e5cb9287b8378b393cde82c
Tirol_JPN <Tirol.JPN@gmail.com>
2019-04-01 19:18:01 +0900

    add ABC111 c solution

--- contest/ABC111/c.py
+++ contest/ABC111/c.py
@@ -1,9 +1,20 @@
 import collections
+
 N = int(input())
 V = list(int(i) for i in input().split())
 even = V[0::2]
 odd =  V[1::2]
-even_count = collections.Counter(even).most_common(1)
-print(even_count[0])
-odd_count = collections.Counter(odd).most_common(1)
-print(odd_count[0])
+all_length = len(V)
+even = collections.Counter(even).most_common(2)
+odd = collections.Counter(odd).most_common(2)
+if (even[0][0] != odd[0][0]):
+    print(all_length - even[0][1] - odd[0][1])
+else:
+    if(len(even) == 1 and len(odd) == 1):
+        print(all_length // 2)
+    elif(len(even)==1):
+        print(all_length // 2 - odd[1][1])
+    elif(len(odd)==1):
+        print(all_length // 2 - even[1][1])    
+    else:
+        print(min(all_length - even[0][1] - odd[1][1], all_length - even[1][1] - odd[0][1]))
