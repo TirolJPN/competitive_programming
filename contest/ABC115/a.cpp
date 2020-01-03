b6e6a234efde8739de9bdd5cc3ca8ddedfe8f596
Tirol_JPN <Tirol.JPN@gmail.com>
2020-01-03 23:00:57 +0900

    add 115 b

--- contest/ABC115/a.cpp
+++ contest/ABC115/a.cpp
@@ -6,7 +6,7 @@ int main(int) {
   int D;
   cin >> D;
   if (D == 25) cout << "Christmas";
-  else if(D == 24) cout << 'Christmas Eve';
-  else if(D == 23) cout << 'Christmas Eve Eve';
-  else if(D == 22) cout << 'Christmas Eve Eve Eve';
+  else if(D == 24) cout << "Christmas Eve";
+  else if(D == 23) cout << "Christmas Eve Eve";
+  else if(D == 22) cout << "Christmas Eve Eve Eve";
 }
